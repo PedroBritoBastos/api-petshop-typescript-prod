165a789aaf8794eccd6829bc3d5e651a74db6ca3
pedro <pedrobritobastos234@gmail.com>
2026-06-30 12:03:07 -0300

    feat(auth): adiciona as rotas de auth

--- src/App.ts
+++ src/App.ts
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 import path from "path";
 
 import { ClientRoutes } from "./modules/client/routes/ClientRoutes";
-//import { AuthRoutes } from "./modules/auth/routes/AuthRoutes";
+import { AuthRoutes } from "./modules/auth/routes/AuthRoutes";
 //import { PetRoutes } from "./modules/pet/routes/PetRoutes";
 //import { PetshopServiceRoutes } from "./modules/petshopService/routes/PetshopServiceRoutes";
 
@@ -15,7 +15,7 @@ import { ClientRoutes } from "./modules/client/routes/ClientRoutes";
 export default class App {
   public app: Application;
   private clientRoutes: ClientRoutes = new ClientRoutes();
-  // private authRoutes: AuthRoutes = new AuthRoutes();
+  private authRoutes: AuthRoutes = new AuthRoutes();
   //  private petRoutes: PetRoutes = new PetRoutes();
   // private petshopServiceRoutes: PetshopServiceRoutes = new PetshopServiceRoutes();
 
@@ -38,7 +38,7 @@ export default class App {
 
   private routes(): void {
     this.app.use(this.clientRoutes.router);
-    //  this.app.use(this.authRoutes.router);
+    this.app.use(this.authRoutes.router);
     //  this.app.use(this.petRoutes.router);
     //  this.app.use(this.petshopServiceRoutes.router);
     //  this.app.use("/photos", express.static(path.resolve("src/data/photos")));
