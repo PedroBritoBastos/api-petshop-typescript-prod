d39636e835175aa96cc4dd4f1f68ca952e5c4be4
pedro <pedrobritobastos234@gmail.com>
2026-06-30 08:56:42 -0300

    feature(auth): adiciona JwtProvider

---
+++ src/shared/auth/JwtProvider.ts
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken";
+import { Request } from "express";
+import "dotenv/config";
+
+/**
+ * this class generates a jwt
+ */
+export class JwtProvider {
+  private static secret = process.env.JWT_SECRET;
+
+  public static generateToken(payload: object): string {
+    if (!JwtProvider.secret) throw new Error("Não há secret.");
+
+    return jwt.sign(payload, JwtProvider.secret, {
+      expiresIn: "1h",
+    });
+  }
+
+  public static verifyToken(token: string): any {
+    if (!this.secret) throw new Error("Não há secret.");
+    return jwt.verify(token, this.secret);
+  }
+
+  public static getClientToken(req: Request): string {
+    const token = req.cookies.token;
+    if (!token) {
+      throw new Error("Token não fornecido.");
+    }
+    return token;
+  }
+}
