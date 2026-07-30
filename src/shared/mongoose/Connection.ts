fe96365f8a477a2694c06364349463dbefc5e041
pedro <pedrobritobastos234@gmail.com>
2026-07-30 15:59:48 -0300

    feat: cria classe com metodo connect para conectar ao mongodb

---
+++ src/shared/mongoose/Connection.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+export class Connection {
+  static async connect(): Promise<void> {
+    try {
+      await mongoose.connect(process.env.MONGODB_DATABASE_URL as string);
+
+      console.log("MongoDB conectado com sucesso.");
+    } catch (error) {
+      console.error("Erro ao conectar ao MongoDB:", error);
+      process.exit(1);
+    }
+  }
+}
