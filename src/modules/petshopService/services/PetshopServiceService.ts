58ff33f2de42359c85e02cc2b139759037f34296
pedro <pedrobritobastos234@gmail.com>
2026-08-03 13:35:11 -0300

    fix: corrige import errado

--- src/modules/petshopService/services/PetshopServiceService.ts
+++ src/modules/petshopService/services/PetshopServiceService.ts
@@ -1,4 +1,4 @@
-import { PetshopService } from "../../../generated/prisma/client";
+import { PetshopService } from "../types/PetshopService";
 import { CreatePetshopServiceDTO } from "../dtos/CreatePetshopServiceDTO";
 import { PetshopServiceRepository } from "../repositories/PetshopServiceRepository";
 import { UpdatePetshopServiceDTO } from "../dtos/UpdatePetshopServiceDTO";
